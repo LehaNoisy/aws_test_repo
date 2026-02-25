acd980899b74645f3a521acb45b4ca0b9103bce5
Alexey <shumilovy@mail.ru>
2026-02-25 12:47:49 +0300

    Update application.py

--- application.py
+++ application.py
@@ -1,18 +1,7 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-
-@app.route('/')
+@application.route("/")
 def index():
-    return "Hello from VERSION2!!!"
-
-
-@app.route('/health')
-def health():
-    return 'ok', 200
-
-
-if __name__ == '__main__':
-    import os
-    app.run(host='0.0.0.0', port=8000)
+    return "Hello from Elastic Beanstalk CI-CD"
